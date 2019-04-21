a52f25a3627395026475b986e8eb1556b7cabdba
Sarah Anderson <saranderson13@gmail.com>
2019-04-21 00:48:04 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,6 @@ require 'pry'
 
 def square_array(array)
   squared = []
-  array.each do |element|
-    squared << element ** 2
-  end
+  array.each { |element| squared << element ** 2 }
   squared
 end
